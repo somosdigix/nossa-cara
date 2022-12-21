0b520be3afc76d4884b2179fc3c9aa70f2162878
Higor Nucci <higornucci@gmail.com>
2022-12-21 12:46:33 -0400

    Inicializacao do SpringBoot

---
+++ src/main/java/br/com/digix/nossacara/NossacaraApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.nossacara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossacaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossacaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digix/nossacara/NossacaraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digix.nossacara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossacaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

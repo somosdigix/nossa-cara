c386a0d54c204ef29cc765be934016969f10f0c7
Tiago Andrade <tiago22900@gmail.com>
2023-03-27 13:17:16 -0400

    EscolaRepositoryTest

---
+++ src/main/java/br/com/digix/nossacara/repository/EscolaRepository.java
@@ -0,0 +1,13 @@
+package br.com.digix.nossacara.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.digix.nossacara.models.Escola;
+
+@Repository
+public interface EscolaRepository extends CrudRepository<Escola, Long>  {
+    List<Escola> findByNomeContaining(String nome);
+}

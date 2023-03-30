c08902b18543afb8d55ddc86e91333b6dcf22def
Higor Nucci <higornucci@gmail.com>
2023-03-30 16:01:34 -0400

    Serviço para buscar comparecimento no refeitorio

--- src/main/java/br/com/digix/nossacara/repository/ReconhecimentoRepository.java
+++ src/main/java/br/com/digix/nossacara/repository/ReconhecimentoRepository.java
@@ -1,23 +1,16 @@
 package br.com.digix.nossacara.repository;
 
 import br.com.digix.nossacara.models.Reconhecimento;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public interface ReconhecimentoRepository extends CrudRepository<Reconhecimento, Long> {
+@Repository
+public interface ReconhecimentoRepository extends CrudRepository<Reconhecimento, Long>, CustomReconhecimentoRepository {
 
     List<Reconhecimento> findAll();
 
-    @Query(value = "select count(DISTINCT r.personId) from Reconhecimento r where cast(r.dataDeCriacao as LocalDate) = :dia and r.deviceKey = :numeroDispositivo")
-    int quantidadeDeReconhecimentosDistintos(@Param("dia") LocalDate dia, @Param("numeroDispositivo") String numeroDispositivo);
-    
     Reconhecimento findFirstByPersonIdOrderByIdDesc(String personId);
 
 }
-
-// select count(DISTINCT person_id) from reconhecimento r
-// where CONVERT(Date, data_de_criacao)<= CAST(N'2022-04-28' AS Date) and device_key = 1;
---
+++ src/main/java/br/com/digix/nossacara/repository/ReconhecimentoRepositoryImpl.java
@@ -0,0 +1,28 @@
+package br.com.digix.nossacara.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ReconhecimentoRepositoryImpl implements CustomReconhecimentoRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public int quantidadeDeReconhecimentosDistintos(LocalDate dia, String numeroDispositivo) {
+        LocalDateTime diaInicio = dia.atStartOfDay();
+        LocalDateTime diaFim = dia.plusDays(1).atStartOfDay();
+        Query singleResult = entityManager.createQuery(
+                        "select count(DISTINCT r.personId) from Reconhecimento r " +
+                                "where r.dataDeCriacao >= :diaInicio and r.dataDeCriacao <= :diaFim and r.deviceKey = :numeroDispositivo")
+                .setParameter("diaInicio", diaInicio)
+                .setParameter("diaFim", diaFim)
+                .setParameter("numeroDispositivo", numeroDispositivo);
+        Object obj = singleResult.getSingleResult();
+        return obj != null ? Integer.parseInt(obj.toString()) : 0;
+    }
+    
+}

b83d3ea00cd7906c21be95b3897a1f8faba8b00c
Higor Nucci <higornucci@gmail.com>
2023-04-14 10:43:28 -0400

    Melhorias na duplicação de código nos testes

--- src/test/java/br/com/digix/nossacara/controllers/PresencaControllerTest.java
+++ src/test/java/br/com/digix/nossacara/controllers/PresencaControllerTest.java
@@ -38,178 +38,141 @@ import br.com.digix.nossacara.utils.JsonUtil;
 @AutoConfigureTestDatabase
 public class PresencaControllerTest {
 
-        @Autowired
-        private ReconhecimentoRepository reconhecimentoRepository;
+    public static final String DEVICE_KEY = "1";
+    public static final String NUMERO_DISPOSITIVO = "84E0F4210D4C607A";
+    public static final LocalDateTime DATA_DE_CRIACAO = LocalDateTime.of(2023, 2, 23, 19, 50, 01);
+    @Autowired
+    private ReconhecimentoRepository reconhecimentoRepository;
 
-        @Autowired
-        private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-        @Autowired
-        private LocalDeEntradaRepository localDeEntradaRepository;
+    @Autowired
+    private LocalDeEntradaRepository localDeEntradaRepository;
 
-        @Autowired
-        private EscolaRepository escolaRepository;
+    @Autowired
+    private EscolaRepository escolaRepository;
 
-        @Autowired
-        private RefeitorioRepository refeitorioRepository;
+    @Autowired
+    private RefeitorioRepository refeitorioRepository;
 
-        @BeforeEach
-        @AfterEach
-        public void deletaDados() {
-                reconhecimentoRepository.deleteAll();
-        }
+    @BeforeEach
+    @AfterEach
+    public void deletaDados() {
+        reconhecimentoRepository.deleteAll();
+    }
 
     @Test
     void deve_buscar_os_comparecimentos_pelo_dia() throws Exception {
-        String deviceKey = "1";
-        LocalDateTime dataDeCriacao = LocalDateTime.of(2023, 2, 23, 19, 50, 01);
-        Reconhecimento reconhecimento1 = new Reconhecimento(deviceKey, "1", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento2 = new Reconhecimento(deviceKey, "2", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento3 = new Reconhecimento(deviceKey, "3", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento4 = new Reconhecimento(deviceKey, "4", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento5 = new Reconhecimento(deviceKey, "5", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento6 = new Reconhecimento(deviceKey, "6", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
-        Reconhecimento reconhecimento7 = new Reconhecimento(deviceKey, "7", dataDeCriacao, "192.168.11.2", "face_0",
-                "https://currentmillis.com/images/milliseconds.png");
+        Reconhecimento reconhecimento1 = criarReconhecimento(DEVICE_KEY, "1");
+        Reconhecimento reconhecimento2 = criarReconhecimento(DEVICE_KEY, "2");
+        Reconhecimento reconhecimento3 = criarReconhecimento(DEVICE_KEY, "3");
+        Reconhecimento reconhecimento4 = criarReconhecimento(DEVICE_KEY, "4");
+        Reconhecimento reconhecimento5 = criarReconhecimento(DEVICE_KEY, "5");
+        Reconhecimento reconhecimento6 = criarReconhecimento(DEVICE_KEY, "6");
+        Reconhecimento reconhecimento7 = criarReconhecimento(DEVICE_KEY, "7");
         reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3,
                 reconhecimento4, reconhecimento5, reconhecimento6, reconhecimento7));
 
-                Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
-                LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, deviceKey, "entradaPrincipal");
-                localDeEntradaRepository.save(localDeEntrada);
-                escolaRepository.save(escola);
+        Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
+        LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, DEVICE_KEY, "entradaPrincipal");
+        localDeEntradaRepository.save(localDeEntrada);
+        escolaRepository.save(escola);
 
-                MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/entradas" + "?dia=" + "2023-02-23"))
-                                .andReturn();
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/entradas" + "?dia=" + "2023-02-23"))
+                .andReturn();
 
-                int status = mvcResult.getResponse().getStatus();
-                assertEquals(HttpStatus.OK.value(), status);
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(HttpStatus.OK.value(), status);
 
-                String content = mvcResult.getResponse().getContentAsString();
-                EntradaResponseDTO entradaDTO = JsonUtil.mapFromJson(content, EntradaResponseDTO.class);
+        String content = mvcResult.getResponse().getContentAsString();
+        EntradaResponseDTO entradaDTO = JsonUtil.mapFromJson(content, EntradaResponseDTO.class);
 
-                Assertions.assertThat(entradaDTO.getQuantidadeEntrada()).isEqualTo(7);
-                Assertions.assertThat(entradaDTO.getQuantidadeAusente()).isEqualTo(3);
-        }
+        Assertions.assertThat(entradaDTO.getQuantidadeEntrada()).isEqualTo(7);
+        Assertions.assertThat(entradaDTO.getQuantidadeAusente()).isEqualTo(3);
+    }
 
-        @Test
-        public void deve_Buscar_Todas_Presencas() throws Exception {
+    private static Reconhecimento criarReconhecimento(String deviceKey, String personId) {
+        return new Reconhecimento(deviceKey, personId, PresencaControllerTest.DATA_DE_CRIACAO, "192.168.11.2", "face_0",
+                "https://currentmillis.com/images/milliseconds.png");
+    }
 
-                String deviceKey = "1";
-                String numeroDispositivo = "84E0F4210D4C607A";
-                LocalDateTime dataDeCriacao = LocalDateTime.of(2023, 2, 23, 19, 50, 01);
+    @Test
+    public void deve_Buscar_Todas_Presencas() throws Exception {
+        Reconhecimento reconhecimento1 = criarReconhecimento(DEVICE_KEY, "1");
+        Reconhecimento reconhecimento2 = criarReconhecimento(DEVICE_KEY, "2");
+        Reconhecimento reconhecimento3 = criarReconhecimento(PresencaControllerTest.NUMERO_DISPOSITIVO, "3");
+        reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
 
-                Reconhecimento reconhecimento1 = new Reconhecimento(deviceKey, "1", dataDeCriacao, "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento2 = new Reconhecimento(deviceKey, "2", dataDeCriacao, "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento3 = new Reconhecimento(numeroDispositivo, "3", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
+        Refeitorio refeitorio = new Refeitorio(1L, PresencaControllerTest.NUMERO_DISPOSITIVO, "Refeitorio Central");
+        LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, DEVICE_KEY, "entradaPrincipal");
+        Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
+        localDeEntradaRepository.save(localDeEntrada);
+        escolaRepository.save(escola);
+        refeitorioRepository.save(refeitorio);
 
-                Refeitorio refeitorio = new Refeitorio(1L, numeroDispositivo, "Refeitorio Central");
-                LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, deviceKey, "entradaPrincipal");
-                Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
-                localDeEntradaRepository.save(localDeEntrada);
-                escolaRepository.save(escola);
-                refeitorioRepository.save(refeitorio);
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas" + "?dia=" + "2023-02-23"))
+                .andReturn();
 
-                MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas" + "?dia=" + "2023-02-23"))
-                                .andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(HttpStatus.OK.value(), status);
 
-                int status = mvcResult.getResponse().getStatus();
-                assertEquals(HttpStatus.OK.value(), status);
+        String content = mvcResult.getResponse().getContentAsString();
+        PresencaResponseDTO presencaDTO = JsonUtil.mapFromJson(content, PresencaResponseDTO.class);
 
-                String content = mvcResult.getResponse().getContentAsString();
-                PresencaResponseDTO presencaDTO = JsonUtil.mapFromJson(content, PresencaResponseDTO.class);
+        Assertions.assertThat(presencaDTO.getQuantidadeEntradaEscola()).isEqualTo(2);
+        Assertions.assertThat(presencaDTO.getQuantidadeEntradaRefeitorio()).isEqualTo(1);
+    }
 
-                Assertions.assertThat(presencaDTO.getQuantidadeEntradaEscola()).isEqualTo(2);
-                Assertions.assertThat(presencaDTO.getQuantidadeEntradaRefeitorio()).isEqualTo(1);
-        }
+    @Test
+    public void deve_Buscar_Presencas_no_Refeitorio() throws Exception {
+        Reconhecimento reconhecimento1 = criarReconhecimento(NUMERO_DISPOSITIVO, "1");
+        Reconhecimento reconhecimento2 = criarReconhecimento(NUMERO_DISPOSITIVO, "2");
+        Reconhecimento reconhecimento3 = criarReconhecimento(NUMERO_DISPOSITIVO, "3");
+        reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
 
-        @Test
-        public void deve_Buscar_Presencas_no_Refeitorio() throws Exception {
-                String numeroDispositivo = "84E0F4210D4C607A";
-                LocalDateTime dataDeCriacao = LocalDateTime.of(2023, 2, 23, 19, 50, 01);
+        Refeitorio refeitorio = new Refeitorio(1L, NUMERO_DISPOSITIVO, "Refeitorio Central");
+        LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, NUMERO_DISPOSITIVO, "entradaPrincipal");
+        Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
+        escolaRepository.save(escola);
+        localDeEntradaRepository.save(localDeEntrada);
+        refeitorioRepository.save(refeitorio);
 
-                Reconhecimento reconhecimento1 = new Reconhecimento(numeroDispositivo, "1", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento2 = new Reconhecimento(numeroDispositivo, "2", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento3 = new Reconhecimento(numeroDispositivo, "3", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/refeitorio" + "?dia=" + "2023-02-23"))
+                .andReturn();
 
-                Refeitorio refeitorio = new Refeitorio(1L, numeroDispositivo, "Refeitorio Central");
-                LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, numeroDispositivo, "entradaPrincipal");
-                Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
-                escolaRepository.save(escola);
-                localDeEntradaRepository.save(localDeEntrada);
-                refeitorioRepository.save(refeitorio);
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(HttpStatus.OK.value(), status);
 
-                MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/refeitorio" + "?dia=" + "2023-02-23"))
-                                .andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        RefeitorioResponseDTO presencaDTO = JsonUtil.mapFromJson(content, RefeitorioResponseDTO.class);
 
-                int status = mvcResult.getResponse().getStatus();
-                assertEquals(HttpStatus.OK.value(), status);
+        Assertions.assertThat(presencaDTO.getQuantidadeEntrada()).isEqualTo(3);
+    }
 
-                String content = mvcResult.getResponse().getContentAsString();
-                RefeitorioResponseDTO presencaDTO = JsonUtil.mapFromJson(content, RefeitorioResponseDTO.class);
+    @Test
+    public void deve_Buscar_Presencas_na_Entrada() throws Exception {
+        Reconhecimento reconhecimento1 = criarReconhecimento(NUMERO_DISPOSITIVO, "1");
+        Reconhecimento reconhecimento2 = criarReconhecimento(NUMERO_DISPOSITIVO, "2");
+        Reconhecimento reconhecimento3 = criarReconhecimento(NUMERO_DISPOSITIVO, "3");
+        reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
 
-                Assertions.assertThat(presencaDTO.getQuantidadeEntrada()).isEqualTo(3);
-        }
-
-        @Test
-        public void deve_Buscar_Presencas_na_Entrada() throws Exception {
-                String numeroDispositivo = "84E0F4210D4C607A";
-                LocalDateTime dataDeCriacao = LocalDateTime.of(2023, 2, 23, 19, 50, 01);
-
-                Reconhecimento reconhecimento1 = new Reconhecimento(numeroDispositivo, "1", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento2 = new Reconhecimento(numeroDispositivo, "2", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                Reconhecimento reconhecimento3 = new Reconhecimento(numeroDispositivo, "3", dataDeCriacao,
-                                "192.168.11.2",
-                                "face_0",
-                                "https://currentmillis.com/images/milliseconds.png");
-                reconhecimentoRepository.saveAll(Arrays.asList(reconhecimento1, reconhecimento2, reconhecimento3));
-
-                LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, numeroDispositivo, "entradaPrincipal");
-                Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
-                escolaRepository.save(escola);
-                localDeEntradaRepository.save(localDeEntrada);
+        LocalDeEntrada localDeEntrada = new LocalDeEntrada(1L, NUMERO_DISPOSITIVO, "entradaPrincipal");
+        Escola escola = new Escola(1, "E E Lucia Martins Coelho", 10);
+        escolaRepository.save(escola);
+        localDeEntradaRepository.save(localDeEntrada);
 
 
-                MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/entradas" + "?dia=" + "2023-02-23"))
-                                .andReturn();
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/presencas/entradas" + "?dia=" + "2023-02-23"))
+                .andReturn();
 
-                int status = mvcResult.getResponse().getStatus();
-                assertEquals(HttpStatus.OK.value(), status);
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(HttpStatus.OK.value(), status);
 
-                String content = mvcResult.getResponse().getContentAsString();
-                EntradaResponseDTO presencaDTO = JsonUtil.mapFromJson(content, EntradaResponseDTO.class);
+        String content = mvcResult.getResponse().getContentAsString();
+        EntradaResponseDTO presencaDTO = JsonUtil.mapFromJson(content, EntradaResponseDTO.class);
 
-                Assertions.assertThat(presencaDTO.getQuantidadeEntrada()).isEqualTo(3);
-        }
+        Assertions.assertThat(presencaDTO.getQuantidadeEntrada()).isEqualTo(3);
+    }
 
 }

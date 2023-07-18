466f2d909bf12f7823c10fd83b5e8de514a71a35
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-07-18 15:07:46 -0400

    Implementando migration

--- src/main/resources/db.migration/V1__create-table-aluno.sql
+++ src/main/resources/db.migration/V1__create-table-aluno.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Aluno (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE aluno (
+  id BIGINT IDENTITY(1,1) PRIMARY KEY,
   nome VARCHAR(255) NOT NULL,
   turma VARCHAR(255) NOT NULL,
   turno VARCHAR(255) NOT NULL,
   personId VARCHAR(255) NOT NULL,
   etapaDeEnsino_id BIGINT,
   escola_id NUMERIC(19, 0) NOT NULL,
-  FOREIGN KEY (etapaDeEnsino_id) REFERENCES EtapaDeEnsino(id),
-  FOREIGN KEY (escola_id) REFERENCES Escola(id)
+  FOREIGN KEY (etapaDeEnsino_id) REFERENCES etapa_de_ensino(id),
+  FOREIGN KEY (escola_id) REFERENCES escola(id)
 );
--- src/main/resources/db.migration/V1__create-table-local_de_entrada.sql
+++ src/main/resources/db.migration/V1__create-table-local_de_entrada.sql
@@ -1,7 +1,7 @@
-CREATE TABLE LocalDeEntrada (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE local_de_entrada (
+  id BIGINT IDENTITY(1,1) PRIMARY KEY,
   numeroDispositivo VARCHAR(255) NOT NULL,
   nome VARCHAR(255) NOT NULL,
   escola_id NUMERIC(19, 0) NOT NULL,
-  FOREIGN KEY (escola_id) REFERENCES Escola(id)
+  FOREIGN KEY (escola_id) REFERENCES escola(id)
 );
